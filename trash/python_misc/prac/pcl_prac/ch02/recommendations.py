1276d3595b25f8d4830efbeb341a2fd5a62263a0
ozt-ca <ozt.dei@gmail.com>
2013-10-07 12:34:49 +0900

    PCL prac - kicked off

---
+++ trash/python_misc/prac/pcl_prac/ch02/recommendations.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 07 11:16:59 2013
+
+@author: alabama_win
+"""
+
+critics={'Lisa Rose': {'Lady in the Water':2.5, 'Snakes on a Plane':3.5,
+                       'Just My Luck': 3.0, 'Superman Returns':3.5, 'You, Me and Dupree': 2.5,
+                       'The Night Listener':3.0},
+         'Gene Seymour': {'Lady in the Water':3.0, 'Snakes on a Plane':3.5,
+                          'Just My Luck': 1.5, 'Superman Returns':5.0, 'You, Me and Dupree': 3.0,
+                          'The Night Listener':3.5}
+        }

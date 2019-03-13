eb8edb94d5e5e48882aae754757358271d95a886
Takashi J. OZAKI <ozt.dei@gmail.com>
2019-03-13 15:00:37 +0900

    Add some trivial and forgotten ones

--- r_samples/public_lib/jp/blog_gaussian_process_gausspr_classify.R
+++ r_samples/public_lib/jp/blog_gaussian_process_gausspr_classify.R
@@ -1,13 +1,18 @@
 library(kernlab)
 d <- read.csv('https://raw.githubusercontent.com/ozt-ca/tjo.hatenablog.samples/master/r_samples/public_lib/jp/xor_complex_small.txt', sep = ' ')
-summary(d)
 d$label <- as.factor(d$label-1)
+px <- seq(-4, 4, 0.03)
+py <- seq(-4, 4, 0.03)
+pgrid <- expand.grid(px, py)
+names(pgrid) <- names(d)[-3]
+
 fit <- gausspr(label~., d, type = 'classification')
 out <- predict(fit, newdata = pgrid, type = 'probabilities')
 out_vec <- rep(0, nrow(out))
 for (i in 1:nrow(out)){
   if (out[i, 1] > 0.5) out_vec[i] <- 1
 }
+
 plot(c(), type = 'n', xlim = c(-4, 4), ylim = c(-4, 4), xlab = '', ylab = '')
 par(new = T)
 rect(0, 0, 4, 4, col = '#dddddd')

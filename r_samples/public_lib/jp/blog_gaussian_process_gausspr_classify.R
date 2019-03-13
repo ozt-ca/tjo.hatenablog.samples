075d3daef655d57b72be2bf7f2ac73b4205aeed2
Takashi J. OZAKI <ozt.dei@gmail.com>
2019-03-13 14:54:21 +0900

    GP for classification

---
+++ r_samples/public_lib/jp/blog_gaussian_process_gausspr_classify.R
@@ -0,0 +1,24 @@
+d <- read.csv('https://raw.githubusercontent.com/ozt-ca/tjo.hatenablog.samples/master/r_samples/public_lib/jp/xor_complex_small.txt', sep = ' ')
+summary(d)
+d$label <- as.factor(d$label-1)
+fit <- gausspr(label~., d, type = 'classification')
+out <- predict(fit, newdata = pgrid, type = 'probabilities')
+out_vec <- rep(0, nrow(out))
+for (i in 1:nrow(out)){
+  if (out[i, 1] > 0.5) out_vec[i] <- 1
+}
+plot(c(), type = 'n', xlim = c(-4, 4), ylim = c(-4, 4), xlab = '', ylab = '')
+par(new = T)
+rect(0, 0, 4, 4, col = '#dddddd')
+par(new = T)
+rect(-4, 0, 0, 4, col = '#ffdddd')
+par(new = T)
+rect(-4, -4, 0, 0, col = '#dddddd')
+par(new = T)
+rect(0, -4, 4, 0, col = '#ffdddd')
+par(new = T)
+plot(d[, -3], pch = 19, cex = 2, col = d$label, xlim = c(-4, 4), ylim = c(-4, 4),
+     xlab = '', ylab = '')
+par(new = T)
+contour(px, py, array(out_vec, c(length(px), length(py))),
+        col = 'purple', levels = 0.5, lwd = 3, drawlabels = F)

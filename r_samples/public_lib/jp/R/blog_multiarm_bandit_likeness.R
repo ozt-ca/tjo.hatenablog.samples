3254ff4363742feeb3e1c892338e2cef4a883cae
Takashi J. OZAKI <ozt.dei@gmail.com>
2019-03-20 16:07:33 +0900

    Add some inline comments

--- r_samples/public_lib/jp/R/blog_multiarm_bandit_likeness.R
+++ r_samples/public_lib/jp/R/blog_multiarm_bandit_likeness.R
@@ -1,20 +1,32 @@
+# Create a sample data matrix
 set.seed(101)
 d <- t(rmultinom(100000, 1, c(0.05, 0.05, 0.8, 0.05, 0.025, 0.025)))
 
+# Set a hyper parameter
 inc <- 1e-4
+
+# Initiate an arm probability
 prob <- rep(1/6, 6)
+
+# Set a result vector
 res <- rep(0, nrow(d))
+
+# Iterate multi-arm bandit computation
 for (i in 1:nrow(d)){
-  pred <- rmultinom(1, 1, prob)
-  res[i] <- t(pred) %*% d[i, ]
+  pred <- rmultinom(1, 1, prob) # Predict which arm returns 1
+  res[i] <- t(pred) %*% d[i, ] # Compute an internal prod for answer
+  # if 1, update the arm probability to reinforce
   if (res[i] == 1 && min(prob) > 0){
     prob[which(pred > 0)] <- prob[which(pred > 0)] + inc
     prob[which(pred == 0)] <- prob[which(pred == 0)] - inc/5
   }
+  # if 0, update it to less reinforce
   if (res[i] == 0 && min(prob) > 0){
     prob[which(pred > 0)] <- prob[which(pred > 0)] - inc
     prob[which(pred == 0)] <- prob[which(pred == 0)] + inc/5
   }
+  
+  # Adjust a scale of the arm probability
   if (min(prob) < 0){
     prob <- (prob - min(prob)) / sum(prob)
   }
@@ -23,6 +35,7 @@ for (i in 1:nrow(d)){
   }
 }
 
+# Check and plot a trajectory of reinforcement learning
 res_bin <- rep(0, 1000)
 for (i in 1:1000){
   res_bin[i] <- sum(res[((i-1) * 100 + 1) : (i * 100)]) / 100

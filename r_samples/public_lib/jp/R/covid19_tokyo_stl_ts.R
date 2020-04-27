3ff8ae1f3e002d9f35fa42b9e10a34bec4573682
Takashi J. OZAKI <ozt.dei@gmail.com>
2020-04-27 21:05:43 +0900

    Added some line feeds

--- r_samples/public_lib/jp/R/covid19_tokyo_stl_ts.R
+++ r_samples/public_lib/jp/R/covid19_tokyo_stl_ts.R
@@ -11,8 +11,13 @@ dayseq <- as.data.frame(dayseq)
 names(dayseq) <- 'day'
 d3 <- left_join(dayseq, d2, by = 'day')
 d3[which(is.na(d3$num)), 2] <- 0
-dts <- stl(ts(as.numeric(d3$num), frequency = 7), s.window = "per")
-plot(d3$day, dts$time.series[, 2], main = "Tokyo, daily",xlab = "Date", ylab = "Positive reported", type = "l", ylim = c(-10, 230), col = 'blue', lwd = 3)
+dts <- stl(ts(as.numeric(d3$num), frequency = 7), s.window = 'per')
+plot(d3$day, dts$time.series[, 2], main = 'Tokyo, daily',
+     xlab = 'Date', ylab = 'Positive reported', type = 'l',
+     ylim = c(-10, 230), col = 'blue', lwd = 3)
 par(new = T)
-plot(d3$day, dts$time.series[, 2] + dts$time.series[, 3], main = "Tokyo, daily",xlab = "Date", ylab = "Positive reported", type = "l", col = 'red', ylim = c(-10, 230))
-legend('topleft', legend = c('Trend', 'Trend + Seasonality'), col = c('blue', 'red'), lty = 1, lwd = c(3, 1), ncol = 1)
+plot(d3$day, dts$time.series[, 2] + dts$time.series[, 3],
+     main = 'Tokyo, daily',xlab = 'Date', ylab = 'Positive reported',
+     type = 'l', col = 'red', ylim = c(-10, 230))
+legend('topleft', legend = c('Trend', 'Trend + Seasonality'),
+       col = c('blue', 'red'), lty = 1, lwd = c(3, 1), ncol = 1)

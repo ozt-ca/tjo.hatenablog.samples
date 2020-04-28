90f119941fe1b050a36202de2cc7c6f514135511
Takashi J. OZAKI <ozt.dei@gmail.com>
2020-04-28 09:37:16 +0900

    Added comments

--- r_samples/public_lib/jp/R/covid19_tokyo_stl_ts.R
+++ r_samples/public_lib/jp/R/covid19_tokyo_stl_ts.R
@@ -1,17 +1,30 @@
+# Download the raw CSV file
 d <- read.csv('https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_patients.csv')
+# Extract date, with adding a dummy
 d1 <- data.frame(day = d[, 5], num = 1)
 
+# Aggregate as daily sum
 library(dplyr)
-d1$day <- as.Date(d1$day)
+d1$day <- as.Date(d1$day) # Put date into Date class
+# Aggregate with group-by
 d2 <- d1 %>% group_by(day) %>% summarise(sum(num)) %>% as.data.frame()
 names(d2)[2] <- 'num'
+# Set up a consecutive date vector
 dayseq <- seq(from = as.Date(d2$day[1]), to = as.Date(d2$day[nrow(d2)]), by = 'day')
 dayseq <- as.data.frame(dayseq)
 names(dayseq) <- 'day'
+# Join daily sum over the date vector
 d3 <- left_join(dayseq, d2, by = 'day')
+# Fill NAs by 0
 d3[which(is.na(d3$num)), 2] <- 0
+
+# Model trend and seasonality using stl (loess)
 dts <- stl(ts(as.numeric(d3$num), frequency = 7), s.window = 'per')
+
+# Create a title with start and end dates
 mtitle <- paste0('Tokyo, daily from ', d3$day[1], ' to ', d3$day[nrow(d3)])
+
+# Plot raw, trend and trend + seasonality time series
 plot(d3$day, d3$num, main = mtitle,
      xlab = 'Date', ylab = 'Positive reported', type = 'l',
      ylim = c(-10, 230), col = 'black', lwd = 1)

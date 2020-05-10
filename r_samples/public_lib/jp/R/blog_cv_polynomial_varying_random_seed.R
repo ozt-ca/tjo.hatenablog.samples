0924e9c364a8a8a45c2a4f557db5b8304ace5868
Takashi J. OZAKI <ozt.dei@gmail.com>
2020-05-11 01:30:40 +0900

    Modify variables setting

--- r_samples/public_lib/jp/R/blog_cv_polynomial_varying_random_seed.R
+++ r_samples/public_lib/jp/R/blog_cv_polynomial_varying_random_seed.R
@@ -1,18 +1,24 @@
 library(Metrics) # Just for computing RMSE
 
 n <- 360 # Length of each random walk
+r_mean <- 0 # Mean of each random walk
+r_sd <- 20 # SD of each random walk
+r_int <- 1000 # Intercept of each random walk
+
 x <- 1:n # x vector: a basic component of independent variable
+l_range <- 1:300 # Range of learning dataset
+t_range <- 301:360 # Range of test dataset
 
 q_range <- 101:300 # Range of random seeds: of course you can change this range
-cv <- rep(0, k) # Vector for storing results of polynomial fitting with CV
-ncv <- rep(0, k) # Vector for storing results of polynomial fitting without CV
+cv <- rep(0, length(q_range)) # Vector for storing results of polynomial fitting with CV
+ncv <- rep(0, length(q_range)) # Vector for storing results of polynomial fitting without CV
 
 # Loop for each random seed
 for (q in q_range){
   
   set.seed(q) # Set a random seed
   
-  y <- cumsum(rnorm(360, 0, 20)) + 1000 # Generate a random walk
+  y <- cumsum(rnorm(n, r_mean, r_sd)) + r_int # Generate a random walk
   # Set up a data frame for each total dataset
   d <- data.frame(y = y, 
                   x1 = x, x2 = x^2, x3 = x^3, x4 = x^4, x5 = x^5,
@@ -20,7 +26,7 @@ for (q in q_range){
                   x11 = x^11, x12 = x^12, x13 = x^13, x14 = x^14, x15 = x^15,
                   x16 = x^16, x17 = x^17, x18 = x^18, x19 = x^19, x20 = x^20
   )
-  d_learn <- d[1:300, ] # Learn dataset, 1:300
+  d_learn <- d[l_range, ] # Learn dataset, 1:300
   d_train <- d_learn[1:(round(0.8 * nrow(d_learn))),] # Training
   d_val <- d_learn[(round(0.8 * nrow(d_learn)) + 1):nrow(d_learn),] # Validation
   
@@ -36,15 +42,15 @@ for (q in q_range){
   fit_best <- lm(y ~ ., d_train[, 1:(2 + idx)]) # Best polynomial fitting model
   fit_all <- lm(y ~ ., d_train) # Full polynomial fitting model
   
-  newdata <- d[301:360, ] # Test dataset, 301:360
+  newdata <- d[t_range, ] # Test dataset, 301:360
   # Prediction by the best model
   pred_best <- predict(fit_best, newdata = newdata[, 1:(2 + idx)])
   # Prediction by the full model
   pred_all <- predict(fit_all, newdata = newdata)
   
   # Store results for each random seed
-  cv[q] <- rmse(pred_best, newdata$y)
-  ncv[q] <- rmse(pred_all, newdata$y)
+  cv[q - q_range[1] + 1] <- rmse(pred_best, newdata$y)
+  ncv[q - q_range[1] + 1] <- rmse(pred_all, newdata$y)
 }
 
 # Final evaluation

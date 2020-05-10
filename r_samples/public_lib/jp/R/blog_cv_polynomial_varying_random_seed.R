ef26498ca043ba72b2fca83c2da4ca460f382984
Takashi J. OZAKI <ozt.dei@gmail.com>
2020-05-11 01:20:49 +0900

    Add comments

--- r_samples/public_lib/jp/R/blog_cv_polynomial_varying_random_seed.R
+++ r_samples/public_lib/jp/R/blog_cv_polynomial_varying_random_seed.R
@@ -1,38 +1,53 @@
-library(Metrics)
+library(Metrics) # Just for computing RMSE
 
-n <- 360
-x <- 1:n
+n <- 360 # Length of each random walk
+x <- 1:n # x vector: a basic component of independent variable
 
-k <- 100
-cv <- rep(0, k)
-ncv <- rep(0, k)
+q_range <- 101:300 # Range of random seeds: of course you can change this range
+cv <- rep(0, k) # Vector for storing results of polynomial fitting with CV
+ncv <- rep(0, k) # Vector for storing results of polynomial fitting without CV
 
-for (q in 1:k){
-  set.seed(q)
-  y <- cumsum(rnorm(360, 0, 20)) + 1000
+# Loop for each random seed
+for (q in q_range){
+  
+  set.seed(q) # Set a random seed
+  
+  y <- cumsum(rnorm(360, 0, 20)) + 1000 # Generate a random walk
+  # Set up a data frame for each total dataset
   d <- data.frame(y = y, 
                   x1 = x, x2 = x^2, x3 = x^3, x4 = x^4, x5 = x^5,
                   x6 = x^6, x7 = x^7, x8 = x^8, x9 = x^9, x10 = x^10,
                   x11 = x^11, x12 = x^12, x13 = x^13, x14 = x^14, x15 = x^15,
                   x16 = x^16, x17 = x^17, x18 = x^18, x19 = x^19, x20 = x^20
   )
-  d_learn <- d[1:300, ]
-  d_train <- d_learn[1:(round(0.8 * nrow(d_learn))),]
-  d_val <- d_learn[(round(0.8 * nrow(d_learn)) + 1):nrow(d_learn),]
+  d_learn <- d[1:300, ] # Learn dataset, 1:300
+  d_train <- d_learn[1:(round(0.8 * nrow(d_learn))),] # Training
+  d_val <- d_learn[(round(0.8 * nrow(d_learn)) + 1):nrow(d_learn),] # Validation
   
-  res <- rep(0, 20)
+  # CV procedure
+  res <- rep(0, 20) # 20 steps CV
   for (j in 1:20){
+    # Polynomial fitting with increasing the degree
     fit <- lm(y ~., d_train[, 1:(1 + j)])
-    res[j] <- rmse(d_val$y, predict(fit, d_val[, 1:(1 + j)]))
+    res[j] <- rmse(d_val$y, predict(fit, d_val[, 1:(1 + j)])) # Record RMSE
   }
-  idx <- which(res == min(res))
-  fit_best <- lm(y ~ ., d_train[, 1:(2 + idx)])
-  fit_all <- lm(y ~ ., d_train)
-  newdata <- d[301:360, ]
+  idx <- which(res == min(res)) # Choose the best one with the least RMSE
+  
+  fit_best <- lm(y ~ ., d_train[, 1:(2 + idx)]) # Best polynomial fitting model
+  fit_all <- lm(y ~ ., d_train) # Full polynomial fitting model
+  
+  newdata <- d[301:360, ] # Test dataset, 301:360
+  # Prediction by the best model
   pred_best <- predict(fit_best, newdata = newdata[, 1:(2 + idx)])
+  # Prediction by the full model
   pred_all <- predict(fit_all, newdata = newdata)
+  
+  # Store results for each random seed
   cv[q] <- rmse(pred_best, newdata$y)
   ncv[q] <- rmse(pred_all, newdata$y)
 }
-boxplot(cv, ncv, names = c('With CV', 'Without CV'))
-t.test(cv, ncv)
+
+# Final evaluation
+boxplot(cv, ncv, names = c('With CV', 'Without CV')) # Boxplot
+t.test(cv, ncv) # Welch's two-sample t-test
+wilcox.test(cv, ncv) # Ranksum test

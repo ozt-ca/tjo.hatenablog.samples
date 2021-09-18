3008efa29128a80fba505b32f4294bfc9e7f81b5
Takashi J. OZAKI <ozt.dei@gmail.com>
2021-09-19 01:38:08 +0900

    Added ggplot2 plotting

--- r_samples/public_lib/jp/R/covid19_tokyo_4metrics_plotting.R
+++ r_samples/public_lib/jp/R/covid19_tokyo_4metrics_plotting.R
@@ -1,3 +1,7 @@
+library(ggplot2)
+library(ggsci)
+library(reshape2)
+
 d <- read.csv('https://stopcovid19.metro.tokyo.lg.jp/data/130001_tokyo_covid19_details_testing_positive_cases.csv')
 
 d1 <- d[248:nrow(d), c(4, 5, 7, 8, 12)]
@@ -31,3 +35,24 @@ matplot(dn2[, -1], type = 'l', lty = 1, ylab = '', xaxt = 'n', lwd = 2,
 axis(side = 1, at = xval, labels = xstr)
 legend('topleft', legend = c('Positive', 'Mild & Medium', 'Critical', 'Dead'),
        lty = 1, lwd = 3, col = 1:4, ncol = 2)
+
+df <- melt(dn2)
+df$value <- as.numeric(df$value)
+df$day <- as.Date(df$day)
+
+p <- ggplot(data = df,
+            mapping = aes(x = day, y = value,
+                          colour = variable, group = variable)) +
+  geom_line(stat = 'identity', size = 1) +
+  scale_colour_brewer(palette = 'Dark2') +
+  scale_x_date(breaks = function(x) seq.Date(from = df[1, 1],
+                                             to = max(x), by = '45 days'),
+               date_labels = '%y-%m-%d') +
+  ggtitle(label = 'Normalized values of 4 COVID metrics in Tokyo') +
+  theme(plot.title = element_text(size = 28, hjust = 0.5),
+        legend.title = element_blank(),
+        legend.text = element_text(size = 24),
+        axis.text.x = element_text(size = 12),
+        axis.text.y = element_text(size = 12)) +
+  labs(x = 'Date', y = 'Normalized value')
+print(p)

5c0665fde9cb0a0469ac5977a9b48d59696791d8
Takashi J. OZAKI <ozt.dei@gmail.com>
2025-01-11 16:21:31 +0900

    Add files via upload

--- r_samples/public_lib/jp/R/blog_multicolinearity_housing.R
+++ r_samples/public_lib/jp/R/blog_multicolinearity_housing.R
@@ -1,4 +1,4 @@
-d <- read.csv('housing.csv', sep = '\t')
+d <- read.csv('https://raw.githubusercontent.com/ozt-ca/tjo.hatenablog.samples/refs/heads/master/r_samples/public_lib/jp/R/housing.csv', sep = '\t')
 d$CHAS <- as.factor(d$CHAS)
 d$RAD <- as.integer(d$RAD)
 summary(d)
@@ -14,3 +14,7 @@ summary(d1.lm)
 
 car::vif(d.lm)
 car::vif(d1.lm)
+
+d1.cv.glmnet <- glmnet::cv.glmnet(as.matrix(d1[, -13]), as.matrix(d1[, 13]),
+                                 family = 'gaussian', alpha = 1)
+coef(d1.cv.glmnet, s = d1.cv.glmnet$lambda.min)
